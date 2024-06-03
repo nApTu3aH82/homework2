9e11fa475b8ded00fc8d6e20acb7f36f01ec6041
nApTu3aH82 <rinsy89@gmail.com>
2024-06-03 10:46:13 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_hw = 12
+count_time = 1.5
+course_name = 'Python'
+srednee = count_time / count_hw
+print('Курс:', course_name, ', всего задач:', count_hw, ', затрачено часов:', count_time, ', среднее время выполнения', srednee, 'часа.')
